7ea5bf9fc8d3fda0ad9d60421aeb8ac57ec327c3
Dominion A <dominionai394@gmail.com>
2025-03-03 20:30:30 -0800

    modification of code

--- src/Table/BarOrders.jsx
+++ src/Table/BarOrders.jsx
@@ -1,37 +1,3 @@
-// // src/components/TableManagement/BarOrders.jsx
-// import { useEffect, useState } from 'react';
-// import { fetchBarOrders } from './TableAPI';
-
-// const BarOrders = ({ token }) => {
-//     const [orders, setOrders] = useState([]);
-    
-//     useEffect(() => {
-//         const getOrders = async () => {
-//             const data = await fetchBarOrders(token);
-//             setOrders(data);
-//         };
-//         getOrders();
-//     }, [token]);
-    
-//     return (
-//             <div className="bg-indigo-500 text-white p-4 rounded-lg w-[80%] mx-auto">
-//                 <h2 className="text-lg font-semibold mb-2">Bar Orders</h2>
-//                 <ul>
-//                     {orders.map(order => (
-//                     <li 
-//                         key={order.id} 
-//                         className="p-2 mb-2 bg-indigo-400 rounded-md hover:bg-indigo-500">
-//                         Order {order.order} - {order.quantity}x Product {order.product}
-//                     </li>
-//                     ))}
-//                 </ul>
-//             </div>
-//         );
-// };
-
-// export default BarOrders;
-
-
 // src/components/TableManagement/BarOrders.jsx
 import { useEffect, useState } from 'react';
 import { fetchBarOrders } from './TableAPI';
